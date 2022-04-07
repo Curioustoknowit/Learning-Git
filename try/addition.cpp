341229243ca4a84f3a8c14757597c2c98e8ba1aa
Curioustoknowit <thinkbefore673@gmail.com>
2022-04-07 20:36:34 +0530

    Added addition.cpp File

---
+++ try/addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int a, b, c;
+   cout << "Enter two Numbers to ADD: " << endl;
+   cin >> a >> b;
+
+   c = a + b;
+   cout << "Addition of both the numbers is: " << c << endl;
+
+   return 0;
+}
