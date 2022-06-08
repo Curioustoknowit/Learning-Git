f5d0e90f59e4d41d136c0b09c483375e60f65b5f
Curioustoknowit <98939974+Curioustoknowit@users.noreply.github.com>
2022-06-08 19:15:18 +0530

    Update test.cpp

--- try/test.cpp
+++ try/test.cpp
@@ -3,10 +3,7 @@ using namespace std;
 
 int main(){
 
-cout << "If your looking this file then it is successfully Uploaded in github";
+   cout << "If your looking this file then it is successfully Uploaded in github";
 
 return 0;
 }
-
-
-
